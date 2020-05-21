73f735749f0f9cf1ea6affa036a7d55c23255510
Azriel Hoh <azriel91@gmail.com>
2020-05-21 14:41:33 +1200

    Uses regular closure and async block in example.

--- examples/window.rs
+++ examples/window.rs
@@ -1,4 +1,3 @@
-#![feature(async_closure)]
 use winit::{event::WindowEvent, event_loop::EventLoop, window::WindowBuilder};
 use winit_async::{EventAsync as Event, EventLoopAsync};
 
@@ -6,28 +5,30 @@ fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run_async(async move |mut runner| 'main: loop {
-        runner.wait().await;
+    event_loop.run_async(move |mut runner| async move {
+        'main: loop {
+            runner.wait().await;
 
-        let mut recv_events = runner.recv_events().await;
-        while let Some(event) = recv_events.next().await {
-            match event {
-                Event::WindowEvent {
-                    event: WindowEvent::CloseRequested,
-                    ..
-                } => {
-                    break 'main;
+            let mut recv_events = runner.recv_events().await;
+            while let Some(event) = recv_events.next().await {
+                match event {
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        ..
+                    } => {
+                        break 'main;
+                    }
+                    _ => println!("{:?}", event),
                 }
-                _ => println!("{:?}", event),
             }
-        }
 
-        window.request_redraw();
+            window.request_redraw();
 
-        let mut redraw_requests = recv_events.redraw_requests().await;
-        while let Some(window_id) = redraw_requests.next().await {
-            println!("redraw {:?}", window_id);
+            let mut redraw_requests = recv_events.redraw_requests().await;
+            while let Some(window_id) = redraw_requests.next().await {
+                println!("redraw {:?}", window_id);
+            }
+            println!();
         }
-        println!();
     })
 }
